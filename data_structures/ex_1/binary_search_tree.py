0f4ea52309a827bfa6d736550cb19f5ac70c19a0
emaquino44 <prexy44@yahoo.com>
2018-09-07 10:22:49 -0700

    completed ex1 - DFS and BFS

--- data_structures/ex_1/binary_search_tree.py
+++ data_structures/ex_1/binary_search_tree.py
@@ -2,13 +2,26 @@ class BinarySearchTree:
   def __init__(self, value):
     self.value = value
     self.left = None
-    self.right = None
+    self.right= None
 
   def depth_first_for_each(self, cb):
-
+    cb(self.value) #start at root
+    if self.left:  #transverse left side
+      self.left.depth_first_for_each(cb)  #mark that they have been visited
+    if self.right: #then transverse the right side
+      self.right.depth_first_for_each(cb)  #mark that they have been visited
+    return
 
   def breadth_first_for_each(self, cb):
-    pass
+    nodes = [self ] #keep track of nodes
+    while len(nodes) > 0:  #loop through checking nodes
+      node = nodes.pop(0)  #pop first node from queue
+      cb(node.value)
+      if node.left:  # if checking on left, add to queue of nodes
+        nodes.append(node.left)
+      if node.right:  #if checking on right, add to queue of nodes
+        nodes.append(node.right)
+
 
   def insert(self, value):
     new_tree = BinarySearchTree(value)
